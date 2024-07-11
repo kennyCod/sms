13cd595b4e4be0ad5c907ad67bb8ec9deba1b8bb
Kenny <okewolek@gmail.com>
2024-07-11 19:21:41 +0100

    Initial commit by Kenny

---
+++ src/main/java/doyenify/sms/api/testRestAPI.java
@@ -0,0 +1,12 @@
+package doyenify.sms.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testRestAPI {
+    @GetMapping("/doyenify")
+    public String helloWorld(){
+    return "Welcome to the Doyenify course!";
+    }
+}
